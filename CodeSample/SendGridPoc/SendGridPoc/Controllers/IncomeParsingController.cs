21c3ca628c3338c635d1d5857bdeeb17fcb98608
Batista <x-dkmirjoa@carlsberggroup.com>
2021-10-18 13:48:01 +0100

    sendgrid inbound parsing techalong

---
+++ CodeSample/SendGridPoc/SendGridPoc/Controllers/IncomeParsingController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using SendGridPoc.Models;
+using StrongGrid;
+using System;
+using System.Collections.Generic;
+
+namespace SendGridPoc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IncomeParsingController : ControllerBase
+    {
+        private const string _cacheKey = "MessageStorage";
+        private readonly ILogger<IncomeParsingController> _logger;
+        private readonly IMemoryCache _cache;
+
+        public IncomeParsingController(ILogger<IncomeParsingController> logger, IMemoryCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public IEnumerable<IncomingMessage> Get()
+        {
+            List<IncomingMessage> cacheEntry;
+            if (!_cache.TryGetValue(_cacheKey, out cacheEntry))
+                cacheEntry = new List<IncomingMessage>();
+            return cacheEntry;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            try
+            {
+                var parser = new WebhookParser();
+                var inboundMail = parser.ParseInboundEmailWebhook(Request.Body);
+
+                List<IncomingMessage> cacheEntry;
+                if (!_cache.TryGetValue(_cacheKey, out cacheEntry))
+                    cacheEntry = new List<IncomingMessage>();
+
+                var email = new IncomingMessage
+                {
+                    //Dkim = inboundMail.Dkim,
+                    To = inboundMail.To[0].Email.ToString(),
+                    //Html = inboundMail.Html,
+                    From = inboundMail.From.Email.ToString(),
+                    Text = inboundMail.Text,
+                    //SenderIp = inboundMail.SenderIp,
+                    //Envelope = inboundMail.Envelope.ToString(),
+                    Attachments = inboundMail.Attachments.Length,
+                    Subject = inboundMail.Subject,
+                    //Charsets = inboundMail.Charsets.ToString(),
+                    //Spf = inboundMail.Spf,
+                };
+                cacheEntry.Add(email);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromDays(365));
+
+                _cache.Set(_cacheKey, cacheEntry, cacheEntryOptions);
+                _logger.LogInformation("Received email " + inboundMail.Subject);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error", ex);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CodeSample/SendGridPoc/SendGridPoc/Models/IncomingMessage.cs
@@ -0,0 +1,60 @@
+﻿namespace SendGridPoc.Models
+{
+    public class IncomingMessage
+    {
+        /// <summary>
+        /// The Domain Keys Identified Email code for the email
+        /// </summary>
+        public string Dkim { get; set; }
+
+        /// <summary>
+        /// The email address that the email was sent to
+        /// </summary>
+        public string To { get; set; }
+
+        /// <summary>
+        /// The HTML body of the email
+        /// </summary>
+        public string Html { get; set; }
+
+        /// <summary>
+        /// The email address the email was sent from
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// The Text body of the email
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The Ip address of the sender of the email
+        /// </summary>
+        public string SenderIp { get; set; }
+
+        /// <summary>
+        /// A JSON string containing the SMTP envelope. This will have 2 variables: to, which is an array of recipients, and from, which is the return path for the message.
+        /// </summary>
+        public string Envelope { get; set; }
+
+        /// <summary>
+        /// Number of attachments included in email
+        /// </summary>
+        public int Attachments { get; set; }
+
+        /// <summary>
+        /// The subject of the email
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// A JSON string containing the character sets of the fields extracted from the message.
+        /// </summary>
+        public string Charsets { get; set; }
+
+        /// <summary>
+        /// The results of the Sender Policy Framework verification of the message sender and receiving IP address.
+        /// </summary>
+        public string Spf { get; set; }
+    }
+}
